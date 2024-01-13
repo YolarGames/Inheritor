d0f5dd3f9f79f65164270ff70c715848ad6ae440
Nikita Ivanin <nikitaivanin93@gmail.com>
2024-01-13 17:03:41 +0100

    add initial canvas setup

---
+++ Assets/_Code/CanvasScalerSetup.cs
@@ -0,0 +1,21 @@
+﻿using Configs;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasScaler))]
+public sealed class CanvasScalerSetup : MonoBehaviour
+{
+	[SerializeField] private CanvasScalerConfig _config;
+	private CanvasScaler _scaler;
+
+	private void Awake()
+	{
+		_scaler = GetComponent<CanvasScaler>();
+		
+		_scaler.uiScaleMode = _config.ScaleMode;
+		_scaler.referenceResolution = _config.ReferenceResolution;
+		_scaler.screenMatchMode = _config.MatchMode;
+		_scaler.matchWidthOrHeight = _config.MatchWidthHeight;
+		_scaler.referencePixelsPerUnit = _config.ReferencePixelsPerUnit;
+	}
+}
