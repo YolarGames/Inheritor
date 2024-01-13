c0c846a97e1e40e9043c3c2390cfab6b97cf2d6e
Nikita Ivanin <nikitaivanin93@gmail.com>
2024-01-13 16:55:12 +0100

    add => new input system package set => build target platform -> Android add => CanvasSetupConfig
    Signed-off-by: Nikita Ivanin <nikitaivanin93@gmail.com>

---
+++ Assets/_Code/Configs/CanvasScalerConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Configs
+{
+	[CreateAssetMenu(fileName = "CanvasSetupConfig", menuName = "Configs/CanvasSetupConfig")]
+	public sealed class CanvasSetupConfig : ScriptableObject
+	{
+		[SerializeField] private RenderMode _renderMode;
+		[SerializeField] private Vector2Int _targetResolution;
+	}
+}
---
+++ _Code/CanvasSetupConfig.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CanvasSetupConfig", menuName = "Configs/CanvasSetupConfig")]
+public sealed class CanvasSetupConfig : ScriptableObject
+{
+	[SerializeField] private Vector2Int _targetResolution;
+		
+}
