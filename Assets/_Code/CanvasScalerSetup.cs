33d7af3243bc02d86d9e287e8e6a9511ea3d6d25
Nikita Ivanin <nikitaivanin93@gmail.com>
2024-01-13 17:31:11 +0100

    add => can set value from config

--- Assets/_Code/CanvasScalerSetup.cs
+++ Assets/_Code/CanvasScalerSetup.cs
@@ -8,10 +8,14 @@ public sealed class CanvasScalerSetup : MonoBehaviour
 	[SerializeField] private CanvasScalerConfig _config;
 	private CanvasScaler _scaler;
 
-	private void Awake()
+	private void Awake() =>
+		SetValuesFromConfig();
+
+	[ContextMenu("Set values from config")]
+	private void SetValuesFromConfig()
 	{
 		_scaler = GetComponent<CanvasScaler>();
-		
+
 		_scaler.uiScaleMode = _config.ScaleMode;
 		_scaler.referenceResolution = _config.ReferenceResolution;
 		_scaler.screenMatchMode = _config.MatchMode;
