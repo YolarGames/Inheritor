beb68ab90ff1cfd239fccf1699a231a5d9c47178
Nikita Ivanin <nikitaivanin93@gmail.com>
2024-01-13 16:56:54 +0100

    add => new input system package set => build target platform -> Android add => CanvasSetupConfig

--- Assets/_Code/Configs/CanvasScalerConfig.cs
+++ Assets/_Code/Configs/CanvasScalerConfig.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Configs
 {
-	[CreateAssetMenu(fileName = "CanvasSetupConfig", menuName = "Configs/CanvasSetupConfig")]
-	public sealed class CanvasSetupConfig : ScriptableObject
+	[CreateAssetMenu(fileName = "CanvasScalerConfig", menuName = "Configs/CanvasScalerConfig")]
+	public sealed class CanvasScalerConfig : ScriptableObject
 	{
-		[SerializeField] private RenderMode _renderMode;
-		[SerializeField] private Vector2Int _targetResolution;
+		[SerializeField] private ScaleMode _scaleMode;
+		[SerializeField] private Vector2Int _referenceResolution;
+		[SerializeField] private CanvasScaler.ScreenMatchMode _matchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+		[SerializeField, Range(0f, 1f)] private float _matchWidthHeight;
+		[SerializeField] private float _referencePixelsPerUnit;
+
+		public ScaleMode ScaleMode => _scaleMode;
+		public Vector2Int ReferenceResolution => _referenceResolution;
+		public CanvasScaler.ScreenMatchMode MatchMode => _matchMode;
+		public float MatchWidthHeight => _matchWidthHeight;
+		public float ReferencePixelsPerUnit => _referencePixelsPerUnit;
 	}
 }
