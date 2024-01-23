e6704e8d987bb256d56cebe67e34cfa2d7d5fa5d
Nikita Ivanin <nikitaivanin93@gmail.com>
2024-01-23 11:35:21 +0100

    add => new scene flow

---
+++ Assets/CodeBase/GameCore/GameServices/ConfigService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Configs;
+using UnityEngine;
+
+namespace GameCore.GameServices
+{
+	public sealed class ConfigService : IConfigService
+	{
+		public AssetServiceConfig AssetServiceConfig { get; private set; }
+
+		public async Task Init()
+		{
+			AssetServiceConfig = await LoadConfig<AssetServiceConfig>();
+		}
+
+		private async Task<TConfig> LoadConfig<TConfig>() where TConfig : ScriptableObject
+		{
+			Debug.Log($"{GetType().Name} loading {typeof(TConfig).Name} config");
+
+			ResourceRequest request = Resources.LoadAsync<TConfig>(GetConfigPath<TConfig>());
+
+			while (!request.isDone)
+				await Task.Yield();
+
+			return request.asset as TConfig;
+		}
+
+		private static string GetConfigPath<TConfig>() where TConfig : ScriptableObject =>
+			typeof(TConfig).ToString().Replace('.', '/');
+	}
+}
---
+++ Assets/CodeBase/GameCore/GameStateMachine/GameStateMachine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using GameCore.GameServices;
+using Roots;
+
+namespace GameCore.GameStateMachine
+{
+	public sealed class GameStateMachine
+	{
+		private static GameStateMachine _instance;
+		public static GameStateMachine Instance => _instance ??= new GameStateMachine();
+
+		private ServiceLocator _serviceLocator;
+		private IExitableState _currentState;
+		private Dictionary<System.Type, IExitableState> _states;
+
+		public void InitStateMachine(LoadingScreenRoot loadingScreen)
+		{
+			_states = new Dictionary<System.Type, IExitableState>
+			{
+				[typeof(StateBootstrap)] = new StateBootstrap(),
+				[typeof(StateLoadProgress)] = new StateLoadProgress(),
+				[typeof(StateLoadLevel)] = new StateLoadLevel(loadingScreen),
+			};
+		}
+
+		public void Enter<TState>() where TState : class, IState =>
+			ChangeState<TState>().Enter();
+
+		public void Enter<TState, TPayload>(TPayload payload) where TState : class, IStatePayloaded<TPayload> =>
+			ChangeState<TState>().Enter(payload);
+
+		private TState ChangeState<TState>() where TState : class, IExitableState
+		{
+			_currentState?.Exit();
+
+			var state = GetState<TState>();
+			_currentState = state;
+
+			return state;
+		}
+
+		private TState GetState<TState>() where TState : class, IExitableState =>
+			_states[typeof(TState)] as TState;
+	}
+}
