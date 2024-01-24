4682ada94272a9a91c2854739f90fb06869ef1c5
Nikita Ivanin <nikitaivanin93@gmail.com>
2024-01-24 21:43:10 +0100

    add UIToolkit MainMenu

---
+++ Assets/CodeBase/Roots/UiMainMenuRoot.cs
@@ -0,0 +1,44 @@
+using SceneInjection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Roots
+{
+	public sealed class UiMainMenuRoot : ASceneRoot
+	{
+		[SerializeField] private UIDocument _mainMenuDoc;
+		[SerializeField] private StyleSheet _styleSheet;
+
+		private const string PLAY = "play";
+		private const string SETTINGS = "settings";
+		private const string EXIT = "exit";
+
+		public override void Go()
+		{
+			VisualElement root = _mainMenuDoc.rootVisualElement;
+			var play = root.Q<Button>(PLAY);
+			var settings = root.Q<Button>(SETTINGS);
+			var exit = root.Q<Button>(EXIT);
+
+			play.RegisterCallback(new EventCallback<ClickEvent>(LoadGame));
+			settings.RegisterCallback(new EventCallback<ClickEvent>(GoToSettings));
+			exit.RegisterCallback(new EventCallback<ClickEvent>(ExitGame));
+		}
+
+		private async void LoadGame(ClickEvent evt) =>
+			await SceneManagerInstance.StartNewScene<GameRoot>();
+
+		private void GoToSettings(ClickEvent evt) =>
+			Debug.Log("Go to settings");
+
+		private void ExitGame(ClickEvent evt) =>
+#if UNITY_EDITOR
+			EditorApplication.ExitPlaymode();
+#else
+			Application.Quit();
+#endif
+
+		private void OnDisable() { }
+	}
+}
