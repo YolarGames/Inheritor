0417cdead8f5b5004e31a60c2df4b52862a2251c
Yo1ar <nikitaivanin93@gmail.com>
2024-03-13 20:45:17 +0100

    add: farming ui

---
+++ Assets/GameBuildMenuRoot.cs
@@ -0,0 +1,13 @@
+using InheritorCode.SceneInjection;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameBuildMenuRoot : ASceneRoot
+{
+	[SerializeField] private UIDocument _document;
+
+	public override void Go()
+	{
+		base.Go();
+	}
+}
